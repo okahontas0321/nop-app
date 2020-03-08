3aed78c825888d1a0b53329ff265b66c92ded556
okahontas0321 <okahontas0321@ezweb.ne.jp>
2020-03-08 21:20:23 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Machine.create!(
+Machine.create(
   [
     {
       name:  'No.01'
